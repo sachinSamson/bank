178959d4ab6dcd07dd59a53f1ace46b17a240ee7
joe <sachinsaravana96@gmail.com>
2020-04-03 11:19:10 +0530

    rebalance

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,8 +5,11 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { FormsModule }    from '@angular/forms';
 import {CheckboxModule} from 'primeng/checkbox';
+import {DropdownModule} from 'primeng/dropdown';
 import { HeaderComponent } from './header/header.component';
 import { SidebarComponent } from './sidebar/sidebar.component';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {InputTextModule} from 'primeng/inputtext';
 
 @NgModule({
   declarations: [
@@ -15,8 +18,8 @@ import { SidebarComponent } from './sidebar/sidebar.component';
     SidebarComponent
   ],
   imports: [
-    BrowserModule,
-    AppRoutingModule,FormsModule,CheckboxModule
+    BrowserModule,BrowserAnimationsModule,
+    AppRoutingModule,FormsModule,CheckboxModule,DropdownModule,InputTextModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/sidebar/sidebar.component.ts
+++ src/app/sidebar/sidebar.component.ts
@@ -6,9 +6,16 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./sidebar.component.scss']
 })
 export class SidebarComponent implements OnInit {
-
-  constructor() { }
-
+ public rebalancedata:any;
+  constructor() { 
+     this.rebalancedata = [
+     {label: 'Quarterly', value: 'quarterly'},
+     {label: 'Buy and Hold', value: 'Buy and Hold'},
+     {label: 'Monthly', value: 'Monthly'},
+     {label: 'Annually', value: 'Annually'},
+     {label: 'Semi Annually', value: 'Semi Annually'}
+     ]; 
+  }
   ngOnInit() {
   }
 
